f3e3b5173776e651c42be2bfcef24947d2b49bc4
newjiang <newjiang@tom.com>
2019-05-17 10:50:01 +0800

    initial

--- order/src/main/java/com/shao/mall/order/OrderApplication.java
+++ order/src/main/java/com/shao/mall/order/OrderApplication.java
@@ -1,7 +1,15 @@
 package com.shao.mall.order;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  * @author newjiang
@@ -9,8 +17,46 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description: 订单模块的启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Profile("dev")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+        }
+    }
+
+    @Profile("pro")
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
+        }
     }
 }
---
+++ register/src/main/java/com/shao/mall/register/RegisterApplication.java
@@ -0,0 +1,18 @@
+package com.shao.mall.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author newjiang
+ * @date 2019/5/17
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+}
